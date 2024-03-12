fb1a9e1a2abce377bda757f47fb13cbd617250e7
Winne Domingues <wdomingues@id.uff.br>
2024-03-12 08:20:50 -0300

    small fix to local env vars 4

--- party_management/views.py
+++ party_management/views.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.shortcuts import redirect, render
 from django.utils import timezone
 from django.core.mail import send_mail
-from local_envs import WPP
 
 from party_guests.forms import GuestForm
 from party_guests.models import Guest
